0078b80e89e4f374ec43b3f975aad884f9feb362
abd96 <abdulhamed.chribati@tu-dortmund.de>
2021-08-24 15:31:58 +0200

    use secrets

--- page1.py
+++ page1.py
@@ -24,9 +24,28 @@ def connect_firebase():
     :returns: TODO
 
     """
-    firebase_config = loadJSON('fireBaseConfig.json')
+    firebase_config = {
+            'apiKey': st.secrets['apiKey'],
+            'authDomain': st.secrets['authDomain'],
+            'projectId': st.secrets['projectId'],
+            'storageBucket': st.secrets['storageBucket'],
+            'messagingSenderId': st.secrets['messagingSenderId'],
+            'appId': st.secrets['appId'],
+            'databaseURL': st.secrets['databaseURL']
 
-    cred = credentials.Certificate('service_account.json')
+            }
+    cred = {
+            'type': st.secrets['type'],
+            'project_id': st.secrets['project_id'],
+            'private_key_id': st.secrets['private_key_id'],
+            'private_key': st.secrets['private_key'],
+            'client_email': st.secrets['client_email'],
+            'client_id': st.secrets['client_id'],
+            'auth_uri': st.secrets['auth_uri'],
+            'token_uri': st.secrets['token_uri'],
+            'auth_provider_x509_cert_url': st.secrets['auth_provider_x509_cert_url'],
+            'client_x509_cert_url': st.secrets['client_x509_cert_url']
+            }
 
     default_app = None
     if not firebase_admin._apps:
